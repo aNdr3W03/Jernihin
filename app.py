18fcc44d2171af948bd8817b019c12a1b8686eed
aNdr3W03 <andrewbjamesie@yahoo.com>
2022-12-02 21:37:59 +0700

    update project folder

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
-from app.routes import app
+import pickle
+import numpy as np
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+model = pickle.load(open('model/model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+
+    return render_template('index.html', result=prediction[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,18 @@
-import pickle
-import numpy as np
-from app import app
-from flask import render_template, request
+# import pickle
+# import numpy as np
+# from app import app
+# from flask import render_template, request
 
-model = pickle.load(open('app/model/model.pkl', 'rb'))
+# model = pickle.load(open('app/model/model.pkl', 'rb'))
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    features = [float(x) for x in request.form.values()]
-    final_features = [np.array(features)]
-    prediction = model.predict(final_features)
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     features = [float(x) for x in request.form.values()]
+#     final_features = [np.array(features)]
+#     prediction = model.predict(final_features)
 
-    return render_template('index.html', result=prediction[0])
+#     return render_template('index.html', result=prediction[0])
