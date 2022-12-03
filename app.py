98ac94563e8d0bb1ab094900f48f4c36095b29de
aNdr3W03 <andrewbjamesie@yahoo.com>
2022-12-03 12:12:07 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
-from app.routes import app
 import os
+import pickle
+import numpy as np
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+model = pickle.load(open('model/model.pkl', 'rb'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+
+    return render_template('index.html', result=prediction[0])
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
