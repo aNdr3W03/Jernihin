176c0b5a463eeeb2c384e79772c047969df1a348
aNdr3W03 <andrewbjamesie@yahoo.com>
2022-12-06 00:11:52 +0700

    update html structure

--- app.py
+++ app.py
@@ -10,13 +10,17 @@ model = pickle.load(open('model/model.pkl', 'rb'))
 def index():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict')
 def predict():
+    return render_template('predict.html')
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
     features = [float(x) for x in request.form.values()]
     final_features = [np.array(features)]
     prediction = model.predict(final_features)
 
-    return render_template('index.html', result=prediction[0])
+    return render_template('predict.html', result=prediction[0])
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=8080))
